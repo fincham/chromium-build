5e31dc59012d5982382b3847e8484165d5b9f577
Michael Fincham <michael@hotplate.co.nz>
2019-12-30 21:02:18 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -14,13 +14,12 @@ mount -t devpts none /dev/pts
 
 # Set up an unprivileged user to build with
 useradd build
+mkdir -p /home/build/src
+chown -R build:build /home/build/build
+
 cd /home/build
-mkdir -p build/src
-chown -R build:build build
-su - build -c 'git clone --recurse-submodules https://github.com/ungoogled-software/ungoogled-chromium-debian.git'
-cd ungoogled-chromium-debian
-su - build -c 'git checkout --recurse-submodules debian_buster'
-cd ..
+su build -c 'cd /home/build; git clone --recurse-submodules https://github.com/ungoogled-software/ungoogled-chromium-debian.git'
+su build -c 'cd /home/build/ungoogled-chromium-debian; git checkout --recurse-submodules debian_buster'
 
 cp -pr ungoogled-chromium-debian/debian build/src/
 cd build/src
@@ -30,14 +29,14 @@ echo "deb https://deb.debian.org/debian/ buster-backports main" > /etc/apt/sourc
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -t buster-backports -y llvm-8 clang-8 equivs
 
-su - build -c './debian/rules setup-debian'
+su build -c './debian/rules setup-debian'
 
 # install remaining requirements to build Chromium
 mk-build-deps -i debian/control
 rm ungoogled-chromium-build-deps_*.deb
 
 # download and unpack Chromium sources (this will take some time)
-su - build -c './debian/rules setup-local-src'
+su build -c './debian/rules setup-local-src'
 
 # start building
-su - build -c 'dpkg-buildpackage -b -uc'
+su build -c 'dpkg-buildpackage -b -uc'
