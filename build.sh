b499d1529a020b1cc056e265a13efbdbdb60b7b3
Michael Fincham <michael@hotplate.co.nz>
2019-12-31 13:06:47 +1300

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -exv
 
 # Based on https://github.com/ungoogled-software/ungoogled-chromium-debian/blob/debian_buster/README.md
 # Assumes that the packages are already installed thanks to debootstrap etc
+# This script expects to be run in a Debian Buster chroot inside e.g. an Ubuntu CodeBuild container
 
 # reset the broken CodeBuild PATH
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -14,7 +15,7 @@ mkdir -p /dev/pts || true
 mount -t proc proc proc
 mount -t devpts none /dev/pts
 
-
+# grab upstream Ungoogle Chromium packaging
 mkdir -p /working/build/src
 cd /working
 git clone --recurse-submodules https://github.com/ungoogled-software/ungoogled-chromium-debian.git
@@ -30,17 +31,15 @@ echo "deb https://deb.debian.org/debian/ buster-backports main" > /etc/apt/sourc
 apt-get update
 DEBIAN_FRONTEND=noninteractive apt-get install -t buster-backports -y llvm-8 clang-8 equivs
 
+# prepare environment
 ./debian/rules setup-debian
 
-# install remaining requirements to build Chromium
+# install remaining requirements
 mk-build-deps debian/control
 DEBIAN_FRONTEND=noninteractive apt-get -y -o Debug::pkgProblemResolver=yes --no-install-recommends install ./ungoogled-chromium-build-deps_*.deb
 rm ungoogled-chromium-build-deps_*.deb
 
-# fix mysterious use of execdir causing problems
-# sed -e 's/execdir/exec/g' -i debian/rules
-
-# download and unpack Chromium sources (this will take some time)
+# download the actual Chromium source
 ./debian/rules setup-local-src
 
 # start building
